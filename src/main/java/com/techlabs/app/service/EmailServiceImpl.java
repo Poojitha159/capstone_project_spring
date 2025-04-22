bfbc78ceac88ba85cb392af9f4cb7960444828a0
Poojitha159 <85926406+Poojitha159@users.noreply.github.com>
2025-04-22 16:00:44 +0530

    EmailServiceImpl.java

--- src/main/java/com/techlabs/app/service/EmailServiceImpl.java
+++ src/main/java/com/techlabs/app/service/EmailServiceImpl.java
@@ -25,26 +25,20 @@ public class EmailServiceImpl implements EmailService{
 		// TODO Auto-generated method stub
 		
 	}
-//	@Override
-//	public boolean sendEmail(String email, String string, String emailContent) {
-//		// TODO Auto-generated method stub
-//		return false;
-//	}
-//	
+
 	public boolean sendEmail(String recipientEmail, String subject, String content) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom("awasthysuyash@gmail.com"); // Sender's email (your Gmail)
-            message.setTo(recipientEmail); // Recipient's email
+            message.setFrom("awasthysuyash@gmail.com"); 
+            message.setTo(recipientEmail); 
             message.setSubject(subject); // Email subject
             message.setText(content); // Email body
 
-            mailSender.send(message); // Send the email
+            mailSender.send(message); 
 
             // Email sent successfully
             return true;
         } catch (MailException e) {
-            // Handle the exception, for example log it
             System.err.println("Error sending email: " + e.getMessage());
             return false;
         }
